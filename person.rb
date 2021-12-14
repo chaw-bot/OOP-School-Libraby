48a0b7bd9128c7aab0ed03493d931fa0fff053ee
chaw <77142475+chaw-bot@users.noreply.github.com>
2021-12-14 14:08:53 +0200

    Create a persons class and add all the methods

---
+++ person.rb
@@ -0,0 +1,19 @@
+class Person
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
+  attr_accessor :name, :age
+  attr_reader :id
+
+  def of_age?
+    @age > 18
+  end
+
+  def can_use_services?
+    @age > 18 || @parent_permission == true
+  end
+end
