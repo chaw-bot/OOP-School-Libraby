de3026954ca18b1ecdd087ddd7c32357e63a4ce2
chaw <77142475+chaw-bot@users.noreply.github.com>
2021-12-16 01:35:26 +0200

    Create relationships between Person, Rental and Book class

--- person.rb
+++ person.rb
@@ -1,16 +1,22 @@
 require_relative('./corrector')
 
 class Person
+  attr_accessor :name, :age, :rentals
+  attr_reader :id
+
   def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
     @corrector = Corrector.new
+    @rentals = []
   end
 
-  attr_accessor :name, :age
-  attr_reader :id
+  def add_rental(rental)
+    @rentals.push(rental) unless @rentals.include?(rental)
+    rental.person = self
+  end
 
   def can_use_services?
     @age > 18 || @parent_permission == true
