5996b83ebc68a4d441657feabcf4f9e0aff760bc
chaw <77142475+chaw-bot@users.noreply.github.com>
2021-12-16 01:33:55 +0200

    Create relationships between Classroom and Student classes

--- student.rb
+++ student.rb
@@ -1,9 +1,15 @@
 require_relative('./person')
 
 class Student < Person
+  attr_reader :classroom
+
   def initialize(*args, classroom, **kwargs)
     super(*args, **kwargs)
     @classroom = classroom
+  end
+
+  def classroom=(classroom)
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 
   def play_hooky
