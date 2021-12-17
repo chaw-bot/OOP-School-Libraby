fab31bdd53cf9acd8b28700274e3bdfa081f477a
chaw <77142475+chaw-bot@users.noreply.github.com>
2021-12-17 16:03:19 +0200

    create home page method

--- main.rb
+++ main.rb
@@ -128,4 +128,25 @@ class Methods
 end
 
 class App
+  def self.home_page
+    puts 'Welcome to the OOP School Library App!'
+    puts "\n"
+    puts 'Please choose an option by entering a number: '
+
+    @content = {
+      '1' => 'List all books',
+      '2' => 'List all people',
+      '3' => 'Create a person',
+      '4' => 'Create a book',
+      '5' => 'Create a rental',
+      '6' => 'List all rentals for a given person id',
+      '7' => 'Exit'
+    }
+
+    @content.each do |index, string|
+      puts "#{index} - #{string}"
+    end
+
+    Integer(gets.chomp)
+  end
 end
