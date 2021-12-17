82c40e97d95e7c631630ce5f10304a68ec62897f
chaw <77142475+chaw-bot@users.noreply.github.com>
2021-12-17 16:00:45 +0200

    Create create_rental method

--- main.rb
+++ main.rb
@@ -90,4 +90,25 @@ class Methods
       puts person[:output]
     end
   end
+
+  def create_rental
+    puts 'Select a book from the following list by number: '
+    @books.each_with_index do |book, index|
+      puts "#{index}) #{book[:output]}"
+    end
+    book_selected = Integer(gets.chomp)
+    book_chosen = @books[book_selected][:object]
+
+    puts 'Select a person from the following list by number (not id): '
+    @person_array.each_with_index do |person, index|
+      puts "#{index}) #{person[:output]}"
+    end
+    person_selected = Integer(gets.chomp)
+    person_chosen = @person_array[person_selected][:object]
+
+    print 'Date: '
+    rental_date = gets.chomp
+
+    @rental_array.push(Rental.new(rental_date, book_chosen, person_chosen))
+  end
 end
