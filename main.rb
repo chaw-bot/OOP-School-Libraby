adc40ca2eb2814390b9fdf866e1836e1ae6da72c
chaw <77142475+chaw-bot@users.noreply.github.com>
2021-12-17 15:56:50 +0200

    Initialize empty arrays

---
+++ main.rb
@@ -0,0 +1,13 @@
+require_relative('./book')
+require_relative('./person')
+require_relative('./student')
+require_relative('./teacher')
+require_relative('./rental')
+
+class Methods
+  def initialize
+    @person_array = []
+    @books = []
+    @rental_array = []
+  end
+end
